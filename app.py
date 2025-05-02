7a3416a44d490d1749f66a45ea0a36b1278a9c19
Madala-pavani <137467153+Madala-pavani@users.noreply.github.com>
2025-05-02 16:57:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write("Click the button to generate a unique AI-generated face!")
 
 if st.button("🚀 Generate Image"):
     img = generate_image()
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(6, 6))
     ax.imshow(img)
     ax.axis("off")
     st.pyplot(fig)
